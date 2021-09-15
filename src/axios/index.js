22d6eee2e5aaabbee3e2fa7b2aefc46da6d6897a
lemarchesi09 <lemarchesi9@gmail.com>
2021-09-15 19:47:45 -0300

    https

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:'http://app-emmundose.herokuapp.com/api/',
+    baseURL:'https://app-emmundose.herokuapp.com/api/',
 })
 
 const sendForm = async (data)=>{
