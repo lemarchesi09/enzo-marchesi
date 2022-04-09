2b302e8cb5d9c4df56fe601c2d6b316bee31fa43
lemarchesi09 <lemarchesi9@gmail.com>
2022-04-09 17:15:34 -0300

    Products fixed
    Arregle los botones del products

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -12,35 +12,23 @@ const Products = () =>{
             
     
             <div className="sec2__cont">
-                <Carousel indicators={false} className="car-prod">
+                <Carousel indicators={false} controls={true} className="car-prod">
                     <Carousel.Item>
-                        <Carousel.Caption className="car-text">
+                        <Carousel.Caption className="car-text" >
                         <p>Sed ut perspiciatis</p>
                         <h3>First slide label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <Carousel.Caption>
+                        <Carousel.Caption className="car-text">
                         <p>Sed ut perspiciatis</p>
                         <h3>Second slide label</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
-                {/*<img className="sec2__img" src={ProductsImg} alt="Imagen seccion 2"/>*/}
-                {/* <div className="sec2__text--background">
-                    <div className="sec2__text">
-                        <p>Sed ut perspiciatis</p>
-                        <h2>Nemo Enim</h2>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-                        <div className="sec2__iconos">
-                            <a href="#home"><img src={IconoIzq} alt="icono flecha izquierda"/></a>
-                            <a href="#home"><img src={IconoDer} alt="icono flecha derecha"/></a>
-                        </div>
-                        
-                    </div>
-                </div> */}
+
                 
             </div>                
         </section>
